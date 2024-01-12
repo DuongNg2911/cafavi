094ce5253c1d5995b0788af6093552376e925070
Duong Nguyen (bobdebon) <127082369+DuongNg2911@users.noreply.github.com>
2024-01-12 23:05:30 +0700

    Update App.js

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,10 +1,12 @@
 import Navbar from "../components/navbar";
+import Main from "../components/main";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Navbar></Navbar>
+        <Main></Main>
       </header>
     </div>
   );
